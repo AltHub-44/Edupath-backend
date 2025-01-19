f7ad507ee5bed6f7c3907088118622a0b1c04efc
Akande Lateef <akandelateef0@gmail.com>
2025-01-19 16:33:54 +0100

    add: error helper

--- src/common/helpers.js
+++ src/common/helpers.js
@@ -1,1 +1,10 @@
 //helper functions and utils
+const error = (code, message) => {
+    const error = new Error(message)
+    error.statusCode = code
+    throw error
+}
+
+module.exports = {
+    error
+}
