a952780b2f62ea8e5103fc77564e24cb9d212540
Akande Lateef <akandelateef0@gmail.com>
2025-03-10 22:29:02 +0100

    add: redis get and set function

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,10 +1,25 @@
 //helper functions and utils
+const redisClient = require('../config/redisConfigs')
 const error = (code, message) => {
     const error = new Error(message)
     error.statusCode = code
     throw error
 }
 
+//setting data to cache
+const setCache = (key, data, expiry) => {
+    const response = redisClient.set(key, JSON.stringify(data), { EX: `${expiry}`});
+    return response
+
+}
+//getting cached data
+const getCache = async (key) => {
+    const data = await redisClient.get(key);
+    return data;
+}
+
 module.exports = {
-    error
+    error,
+    getCache,
+    setCache
 }
