64404380f01f00c6af4c1638c0fef89d43b625f2
Akande Lateef <akandelateef0@gmail.com>
2025-03-11 08:38:42 +0100

    fix: cache getter and setter promise set

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -7,15 +7,15 @@ const error = (code, message) => {
 }
 
 //setting data to cache
-const setCache = (key, data, expiry) => {
-    const response = redisClient.set(key, JSON.stringify(data), { EX: `${expiry}`});
+const setCache = async (key, data, expiry) => {
+    const response = await redisClient.set(key, JSON.stringify(data), { EX: `${expiry}`});
     return response
 
 }
 //getting cached data
 const getCache = async (key) => {
     const data = await redisClient.get(key);
-    return data;
+    return JSON.parse(data);
 }
 
 module.exports = {
