dbbbe83d24914f0707a40c60441b4e8dd2863c2e
Akande Lateef <akandelateef0@gmail.com>
2025-03-19 20:20:52 +0100

    fix: token bug fixed

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 const generateToken = (user) => {
-  return jwt.sign({ id: user.id, firstname: user.firstname, lastname: user.lastname, email: user.email, role: user.role }, process.env.JWT_SECRET, {
+  return jwt.sign({ id: user.id, firstname: user.firstName, lastname: user.lastName, email: user.email, role: user.role }, process.env.JWT_SECRET, {
     expiresIn: "1h",
   });
 };
