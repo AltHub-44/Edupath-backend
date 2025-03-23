cbc00bb74d1913b60e6ba078a9a3654504f33983
Akande Lateef <akandelateef0@gmail.com>
2025-03-23 08:13:00 +0100

    add: admin module added

--- router.js
+++ router.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const authRoute = require('./src/modules/auth/routes/authRoutes')
 const quoteRouter = require('./src/modules/quote/routes/quoteRoute')
+const adminRoute = require('./src/modules/admin/routes/adminRoutes')
 
 router.get('/', (req, res) => {
     res.status(200).json({message: "Welcome to Edupath Backend, please redirect to /api"})
@@ -9,5 +10,6 @@ router.get('/', (req, res) => {
 
 router.use('/auth', authRoute)
 router.use('/quote', quoteRouter)
+router.use('/admin', adminRoute)
 
 module.exports = router
