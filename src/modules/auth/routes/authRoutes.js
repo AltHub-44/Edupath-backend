e3d264a20e8a706c7e8da2a3d824e2913f9eceee
Akande Lateef <akandelateef0@gmail.com>
2025-03-23 07:18:39 +0100

    fix:auth validator modularized

--- src/modules/auth/routes/authRoutes.js
+++ src/modules/auth/routes/authRoutes.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const authController = require('../controller/authControllers');
-//const validateRequest = require('../../middlewares/validateRequest'); // Middleware to handle Joi validation
-const { registerSchema, loginSchema } = require('../validators/authValidator');
+const validateRequest = require('../../../middlewares/validateRquest'); // Middleware to handle Joi validation
+const { registerSchema,
+     loginSchema, 
+     changePasswordSchema, 
+     recoverPasswordSchema, 
+     resetPasswordSchema } = require('../validators/authValidator');
 const authMiddleware = require('../../../middlewares/authMiddleware');
 
 const router = express.Router();
 
-router.post('/register', authController.createUser)
-router.post('/login', authController.loginUser)
-router.post('/recover-password', authController.recoverPassword)
-router.post('/reset-password', authController.resetPassword);
-router.post('/change-password', authMiddleware, authController.changePassword)
-// router.post("/register", validateRequest(registerSchema), authController.register)
-// router.post("/login", validateRequest(loginSchema) ,authController.login)
+router.post('/register', validateRequest(registerSchema), authController.createUser);
+router.post('/login', validateRequest(loginSchema), authController.loginUser);
+router.post('/recover-password', validateRequest(recoverPasswordSchema), authController.recoverPassword)
+router.post('/reset-password', validateRequest(resetPasswordSchema), authController.resetPassword);
+router.post('/change-password', authMiddleware, validateRequest(changePasswordSchema), authController.changePassword)
 
 module.exports = router
