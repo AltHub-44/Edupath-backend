17034be309442a4dabdf92b3b14f065966364fde
Akande Lateef <akandelateef0@gmail.com>
2025-04-25 21:20:09 +0100

    add: admin authorize middleware

--- router.js
+++ router.js
@@ -4,6 +4,7 @@ const authRoute = require('./src/modules/auth/routes/authRoutes')
 const quoteRouter = require('./src/modules/quote/routes/quoteRoute')
 const adminRoute = require('./src/modules/admin/routes/adminRoutes')
 const studentRoute = require('./src/modules/student/routes/studentRoutes')
+const { authenticate, adminAuthorize } = require('./src/middlewares/authMiddleware')
 
 router.get('/', (req, res) => {
     res.status(200).json({message: "Welcome to Edupath Backend, please redirect to /api"})
@@ -11,7 +12,7 @@ router.get('/', (req, res) => {
 
 router.use('/auth', authRoute)
 router.use('/quote', quoteRouter)
-router.use('/admin', adminRoute)
+router.use('/admin', authenticate, adminAuthorize, adminRoute)
 router.use('/student', studentRoute)
 
 module.exports = router
--- src/modules/auth/routes/onboardingRoutes.js
+++ src/modules/auth/routes/onboardingRoutes.js
@@ -4,9 +4,9 @@ const onboardingController = require('../../auth/controller/onboardingController
 const authMiddleware = require('../../../middlewares/authMiddleware'); // Adjust path if needed
 
 // Onboarding Routes
-router.post('/', authMiddleware, onboardingController.completeOnboarding);
-router.get('/', authMiddleware, onboardingController.getOnboardingStatus);
-router.post("/complete", authMiddleware, onboardingController.completeOnboarding)
-router.get('/status', authMiddleware, onboardingController.getOnboardingStatus);
+router.post('/', authMiddleware.authenticate, onboardingController.completeOnboarding);
+router.get('/', authMiddleware.authenticate, onboardingController.getOnboardingStatus);
+router.post("/complete", authMiddleware.authenticate, onboardingController.completeOnboarding)
+router.get('/status', authMiddleware.authenticate, onboardingController.getOnboardingStatus);
 
 module.exports = router;
