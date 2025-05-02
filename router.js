323fb63d8e2f05f593f65dcb48eb226b92c41f2e
ibraheemreedwan100@gmail.com <ibraheemreedwan100@gmail.com>
2025-05-02 13:44:26 +0100

    conflict resolution

--- router.js
+++ router.js
@@ -6,6 +6,7 @@ const adminRoute = require('./src/modules/admin/routes/adminRoutes')
 const studentRoute = require('./src/modules/student/routes/studentRoutes')
 const chatRoutes = require('./src/modules/mentor/routes/chatRoutes');
 const progressRoutes = require('./src/modules/mentor/routes/progressRoutes');
+const { authenticate, adminAuthorize } = require('./src/middlewares/authMiddleware')
 
 router.get('/', (req, res) => {
     res.status(200).json({message: "Welcome to Edupath Backend, please redirect to /api"})
@@ -13,7 +14,7 @@ router.get('/', (req, res) => {
 
 router.use('/auth', authRoute)
 router.use('/quote', quoteRouter)
-router.use('/admin', adminRoute)
+router.use('/admin', authenticate, adminAuthorize, adminRoute)
 router.use('/student', studentRoute)
 router.use('/api/chat', chatRoutes)
 router.use('/api/progress', progressRoutes)
