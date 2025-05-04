e16be21a911b841f122dfb1afcc3c2c52d74858c
Akande Lateef <Akandelateef0@gmail.com>
2025-05-04 16:14:04 +0100

    Revert "Mentors"

--- router.js
+++ router.js
@@ -4,11 +4,7 @@ const authRoute = require('./src/modules/auth/routes/authRoutes')
 const quoteRouter = require('./src/modules/quote/routes/quoteRoute')
 const adminRoute = require('./src/modules/admin/routes/adminRoutes')
 const studentRoute = require('./src/modules/student/routes/studentRoutes')
-const chatRoutes = require('./src/modules/mentor/routes/chatRoutes');
-const progressRoutes = require('./src/modules/mentor/routes/progressRoutes');
 const { authenticate, adminAuthorize } = require('./src/middlewares/authMiddleware')
-
-
 
 router.get('/', (req, res) => {
     res.status(200).json({message: "Welcome to Edupath Backend, please redirect to /api"})
@@ -18,7 +14,5 @@ router.use('/auth', authRoute)
 router.use('/quote', quoteRouter)
 router.use('/admin', authenticate, adminAuthorize, adminRoute)
 router.use('/student', studentRoute)
-router.use('/api/chat', chatRoutes)
-router.use('/api/progress', progressRoutes)
 
 module.exports = router
